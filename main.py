7cbd593a4cf103ec2f0a28e2dffaf545932c447b
MaxwellVolz <maxwellvolz@gmail.com>
2021-08-10 02:22:18 -0700

    option to equip looted weapon working

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 from minions import minions
 from dragons import dragons
 
+
 # TODO
 #
 # Randomize Weapon drops within defined range
@@ -72,9 +73,8 @@ class Knight:
             Defense: {self.armor['armor']}
             Weapon: {self.weapon['name']}
             Attack: {self.attack + self.weapon['damage']}
-            Crit: {self.crit_chance + self.weapon['crit_chance']}% for {(self.crit_multiplier + self.weapon['crit_multiplier'])*100}%
+            Crit: {self.crit_chance + self.weapon['crit_chance']}% for {(self.crit_multiplier + self.weapon['crit_multiplier']) * 100}%
         """)
-
 
 
 def roll_damage(dmg, crit_chance, crit_multiplier):
@@ -93,11 +93,35 @@ def loot_corpse(drop_table):
     return loot
 
 
-def equip_gear(bag_of_gear):
+def equip_gear(curr_Knight, bag_of_gear):
     for gear in bag_of_gear:
         if gear['type'] == 'weapon':
-            Knight.weapon = gear
+            # Compare
+            print(f"""
+(A) Replace [{curr_Knight.weapon["name"]}]: 
+    {curr_Knight.weapon["damage"]} dmg  +{curr_Knight.weapon["crit_chance"]}% chance to +crit %{curr_Knight.weapon["crit_multiplier"]} 
+(D) With    [{gear["name"]}]: 
+    {gear["damage"]} dmg  +{gear["crit_chance"]}% chance to +crit %{gear["crit_multiplier"]} 
+""")
+            option = msvcrt.getch()
+
+            if option == 'A' or option == 'a' or option == b'a' or option == b'A':
+                print("TODO: Putting in bag")
+            elif option == 'D' or option == 'd' or option == b'd' or option == b'D':
+                Knight.weapon = gear
+
+
         if gear['type'] == 'armor':
+            # Compare
+            print(f"""
+(A) Replace [{curr_Knight.armor["name"]}]: {curr_Knight.armor["armor"]} armor
+(D) With    [{gear["name"]}]: {gear["armor"]} armor
+""")
+            if option == 'A' or option == 'a' or option == b'a' or option == b'A':
+                print("TODO: Putting in bag")
+            elif option == 'D' or option == 'd' or option == b'd' or option == b'D':
+                Knight.weapon = gear
+
             Knight.armor = gear
 
 
@@ -130,10 +154,12 @@ def combat(enemy):
         enemy_health -= calculated_damage
 
     if Knight.curr_health > 0:
-        print(f"{Knight.name} defeats {enemy_name}. {xp_reward} XP awarded. HP: {Knight.curr_health}/{Knight.max_health}\n")
+        print(
+            f"{Knight.name} defeats {enemy_name}. {xp_reward} XP awarded. HP: {Knight.curr_health}/{Knight.max_health}\n")
         Knight.gold += gold_reward
+
         # Auto equip drops
-        equip_gear(loot_corpse(enemy['loot']))
+        equip_gear(Knight, loot_corpse(enemy['loot']))
         Knight.gain_experience(xp_reward)
     else:
         print(f"{Knight.name} has been slain by {enemy_name}({enemy_health}/{enemy_max_health}).\n")
@@ -143,8 +169,7 @@ def combat(enemy):
 def game_loop():
     # option = input("(A)Attack (S)Sleep (D)Dragon (Q)Equip (W)Sell (E)Shop:")
 
-
-    print("(A)Attack (S)Sleep (D)Dragon (Q)Equip (W)Sell (E)Shop")
+    print("(A)Attack (S)Sleep (D)Dragon (Q)Stats (W)Sell (E)Shop")
     # option = getch.getch()
     option = msvcrt.getch()
 
@@ -161,20 +186,22 @@ def game_loop():
         # print(f"{Knight.name} Health:", Knight.curr_health, " | XP:", Knight.experience)
 
     elif option == 'Q' or option == 'q' or option == b'q' or option == b'Q':
-        # print("Equipping!")
         Knight.stat_sheet()
 
     elif option == 'W' or option == 'w' or option == b'w' or option == b'W':
-        print("Selling!")
+        print("Sell coming soon!")
 
     elif option == 'E' or option == 'e' or option == b'e' or option == b'E':
-        print("Shopping!")
+        print("Shop coming soon!")
 
+    elif option == 'X' or option == 'x' or option == b'x' or option == b'X':
+        exit()
     else:
         print("Incorrect option:", option)
         # exit()
 
     game_loop()
+
 
 new_name = input("Enter your name:")
 if new_name == "": new_name = "Gerald"
